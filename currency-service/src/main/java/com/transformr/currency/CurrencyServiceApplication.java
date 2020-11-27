fdd378cab7274b7803ceaacefc2fdf5dc3bb4435
Tamal Mukhopadhyay <tamal.mukherjee10@gmail.com>
2020-11-28 01:36:23 +0800

    Adding api gateway

---
+++ currency-service/src/main/java/com/transformr/currency/CurrencyServiceApplication.java
@@ -0,0 +1,14 @@
+package com.transformr.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CurrencyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyServiceApplication.class, args);
+	}
+}
