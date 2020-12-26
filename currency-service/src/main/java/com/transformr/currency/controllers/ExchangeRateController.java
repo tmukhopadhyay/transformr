841340c6db68bd976fa7b1b5894268b237ff8f0a
Tamal Mukhopadhyay <tamal.mukherjee10@gmail.com>
2020-12-27 01:16:08 +0800

    Updating to latest versions of spring boot and cloud

--- currency-service/src/main/java/com/transformr/currency/controllers/ExchangeRateController.java
+++ currency-service/src/main/java/com/transformr/currency/controllers/ExchangeRateController.java
@@ -2,7 +2,6 @@ package com.transformr.currency.controllers;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,7 +22,7 @@ public class ExchangeRateController {
 		this.exchangeRateService = exchangeRateService;
 	}
 
-	@GetMapping(path = "/exchange", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+	@GetMapping(path = "/exchange")
 	public Mono<ExchangeRateDto> getExchangeRate(
 		@RequestParam(required = true) String from,
 		@RequestParam(required = true) String to
