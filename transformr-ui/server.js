f641a7a5759712855c20473379e10c060d584c74
Tamal Mukhopadhyay <tamal.mukherjee10@gmail.com>
2021-01-11 23:33:48 +0800

    Serving the index.html everytime

--- transformr-ui/server.js
+++ transformr-ui/server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 5000;
 express()
   .use(compression())
   .use(express.static(__dirname + '/dist', { maxAge: 8640000000000 }))
-  .get('/', (req, res) => res.sendFile('./dist/index.html', { root: __dirname }))
+  .get('/*', (req, res) => res.sendFile('./dist/index.html', { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
