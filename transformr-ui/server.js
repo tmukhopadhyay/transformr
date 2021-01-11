c0e1d614e1a80b17ba12ec78f752bb553c6038be
Tamal Mukhopadhyay <tamal.mukherjee10@gmail.com>
2021-01-11 23:12:57 +0800

    Adding configurations for heroku

---
+++ transformr-ui/server.js
@@ -0,0 +1,9 @@
+var compression = require("compression");
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(compression())
+  .use(express.static(__dirname + '/dist', { maxAge: 8640000000000 }))
+  .get('/', (req, res) => res.sendFile('./dist/index.html', { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
